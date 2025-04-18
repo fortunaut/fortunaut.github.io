88aa69c568fbbd2fa6649a409ef0923117c14c24
fortunaut <me@fortunaut.dev>
2025-04-18 13:13:03 -0700

    Add new favicon

--- the-app/src/App.js
+++ the-app/src/App.js
@@ -1,18 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
 import Header from './layout/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import Donate from './pages/Donate';
 import Blog from './pages/Blog';
+import { Routes, Route } from 'react-router';
 
 function App() {
   return (
     <>
       <div className="App">
         <Header />
-        <main className="App-content">
+        <main className="App-content" data-testid="main-content">
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
@@ -22,7 +21,7 @@ function App() {
         </main>
         <footer>
           <div className="App-footer">
-            This site is maintained by <a href="mailto:me@fortunaut.dev">fortunaut</a>
+            This site is maintained by <a href="https://github.com/fortunaut">fortunaut</a>
             <div><a href="/donate">Support the site</a></div>
           </div>
         </footer>
--- the-app/src/App.test.js
+++ the-app/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders homepage', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const mainComponent = screen.getByTestId("main-content");
+  expect(mainComponent).toBeInTheDocument();
 });
