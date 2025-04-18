cb86a4701c41bdbb3e64737f91876a0c1f609106
fortunaut <me@fortunaut.dev>
2025-04-18 11:59:27 -0700

    update donate msg

--- the-app/src/pages/Donate.js
+++ the-app/src/pages/Donate.js
@@ -10,9 +10,6 @@ const Donate = () => {
       While the site itself doesn't have any costs, gardening supplies certainly do. You're welcome to drop off
       extra supplies or seeds if there's something specific you'd like me to try and write about.
     </p>
-    <p >
-      Alternatively, you can support the site anonymously by sending BTC to the address: bc1qcj pyd2ha pvajuy 2yy7vz e7urn3 au50eu g48yma
-    </p >
   </>);
 };
 
