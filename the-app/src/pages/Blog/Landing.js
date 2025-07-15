c4a331191c4e99b4a4aa0fd30e0879bd36b9b21a
fortunaut <me@fortunaut.dev>
2025-07-14 21:36:15 -0700

    Add groundcherry page. Update house tomato page.

--- the-app/src/pages/Blog/Landing.js
+++ the-app/src/pages/Blog/Landing.js
@@ -5,15 +5,19 @@ const Landing = () => {
     <section id="blog">
       <h3>Intro</h3>
       <p>
-        Welcome to my blog landing page. Here I will share information about what plants I am growing here in
-        a foggy San Francisco neighborhood.
-        More posts coming soon!
+        Welcome to my blog landing page. Here I will share information about what plants I am growing
+        here in a foggy San Francisco neighborhood.
       </p>
       <h3>Posts</h3>
       <ul>
         <li>
           <a href='/blog/housetomato'>
             House Tomato - <ItalicText>Solanum lycopersicum</ItalicText>
+          </a>
+        </li>
+        <li>
+          <a href='/blog/groundcherry'>
+            Peruvian groundcherry - <ItalicText>Physalis peruviana</ItalicText>
           </a>
         </li>
       </ul>
